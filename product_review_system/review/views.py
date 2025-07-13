1a5348ecf71a04dfecb5461832bcda44cff49bf4
kiran-kurian <kirankurian02@gmail.com>
2025-07-13 09:13:55 +0530

    added views file

--- product_review_system/review/views.py
+++ product_review_system/review/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions
+from rest_framework.exceptions import ValidationError
+from .models import Product, Review
+from .serializers import ProductSerializer, ReviewSerializer, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+class ReviewCreateView(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        product_id = self.kwargs['pk']
+        product = Product.objects.get(id=product_id)
+        if Review.objects.filter(product=product, user=self.request.user).exists():
+            raise ValidationError("You have already reviewed this product.")
+        serializer.save(user=self.request.user, product=product)
+
+class ReviewListView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        product_id = self.kwargs['pk']
+        return Review.objects.filter(product__id=product_id)
